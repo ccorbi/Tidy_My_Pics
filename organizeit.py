37c572e5dde76799b8763104e0ce4c4d00677ae8
ccorbi <carles.corbi@gmail.com>
2016-10-15 18:17:12 -0400

    command arguments functionality added

--- organizeit.py
+++ organizeit.py
@@ -1,68 +1,72 @@
 from __future__ import print_function
 
 import os
-from os import walk
+import argparse
 import shutil
-
 import exifread
 
-def moveit(f,target):
-    '''Get the name of the photo and the mouth that was taken and move it '''
-    #Check if the folder exists, if not, it make it
+
+def moveit(f, target):
+    """Get the name of the photo and the mouth that was taken and move it.
+
+    """
+    # Check if the folder exists, if not, it make it
 
     if not os.path.isdir(target):
+        print('Move {} to {}'.format(f, target))
         os.makedirs(target)
 
     shutil.move(f, target)
     return
 
+
 def copyit(f, target):
 
     if not os.path.isdir(target):
-        print('-'*30)
+        print('Copy {} to {}'.format(f, target))
         os.makedirs(target)
 
     shutil.copy2(f, target)
     return
 
-#It goes for each jpg in the run folder
+# It goes for each jpg in the run folder
 
 
-def organize(source_pictures, target_folder, default_folder):
+def organize(messy_pictures, target_folder, default_folder):
 
     if not os.path.isdir(target_folder):
         os.mkdir(target_folder)
 
     # Feed featrues to org photos
-    for mistery_photo in source_pictures:
-        print('*'*40)
-        print(mistery_photo)
+    for mistery_photo in messy_pictures:
+
         exif_data = get_EXIF_features(mistery_photo)
         if exif_data['year'] != None:
-            #copy
-            target_folder_file =  '{0}/{1[year]}/{1[mouth]}/{1[day]}/'.format(target_folder, exif_data)
+            # copy
+            target_folder_file = '{0}/{1[year]}/{1[mouth]}/{1[day]}/'.format(
+                target_folder, exif_data)
 
         else:
-            #copy to miscellanian folder
+            # copy to miscellanian folder
             target_folder_file = '{}'.format(default_folder)
-        print(mistery_photo)
-        print(target_folder_file)
+
         copyit(mistery_photo, target_folder_file)
 
     return
+
 
 def get_EXIF_features(f, features='default'):
 
     exif_data = dict()
 
-    #open in binary mode
-    photo = open(f,'rb')
-    #Read  EXIF data
+    # open in binary mode
+    photo = open(f, 'rb')
+    # Read  EXIF data
     tags = exifread.process_file(photo, details=False)
-    #Extract time
+    # Extract time
 
-    #Quick&Dirty to extract mouth
-    ###NEED TO BE IMPROVED TO SUPORT year
+    # Quick&Dirty to extract mouth
+    # NEED TO BE IMPROVED TO SUPORT year
     try:
         timestamp = tags['EXIF DateTimeOriginal'].values
         d, h = timestamp.split()
@@ -74,7 +78,6 @@ def get_EXIF_features(f, features='default'):
         exif_data['min'] = h.split(':')[1].strip()
         exif_data['sec'] = h.split(':')[2].strip()
 
-
     except:
         # add logg
         # configuration file default photos
@@ -83,7 +86,8 @@ def get_EXIF_features(f, features='default'):
 
     return exif_data
 
-def get_source_material(source_path):
+
+def find_photos(source_path, common_extensions=('JPG', 'CR2', 'ORF')):
     """Walk the source folder and select potential photos by extension.
 
     Parameters
@@ -96,29 +100,70 @@ def get_source_material(source_path):
 
     """
     source_files = list()
-    common_extensions = ['JPG', 'CR2', 'ORF']
-    for (dirpath, dirnames, filenames) in walk(source_path):
+
+    for (dirpath, dirnames, filenames) in os.walk(source_path):
         for f in filenames:
             print(f)
             print(os.path.join(dirpath, f))
-            file_extension = f[-3:]
 
-            if file_extension.upper() in common_extensions:
+            if f.upper().endswith(common_extensions):
                 source_files.append(os.path.join(dirpath, f))
 
     return source_files
 
+
+def get_options():
+    """Get arguments from command line.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+    parser = argparse.ArgumentParser(description="""
+    Simple tool to unmess your photos.
+
+    Usage Demultiplexation:
+    %prog --source /my/messy/photos/ --target /my/tidy/photos/
+
+    """)
+
+    parser.add_argument('-s', '--source', action="store",
+                        dest="source_folder", help='Folder with \
+                        the messy collection of photos', required=True)
+
+    parser.add_argument('-t', '--target', action="store",
+                        dest="target_folder", help='Folder with \
+                        the tidy collection of photos', required=True)
+
+    # Advanced Setup & Optional Arguments
+    # behaivour wiht no info, features, copy or move, duplicates
+    parser.add_argument('-d', '--default_folder', action="store", dest="default_folder",
+                        default='demultiplex', help='Folder to store photos without \
+                        valid information to properlly classify')
+
+    parser.add_argument('-b', '--behaivour', action="store",
+                        dest="files_method", default='copy', type=str,
+                        choices=['copy',
+                                 'move'],
+                        help='What do you want to do with the source photos')
+
+    options = parser.parse_args()
+
+    return options
+
+
 def main():
 
-    source_path = './'
-    default_folder = './To_class/'
-    target_folder =  './tidy'
+    # Read argtments & defaults values
+    opts = get_options()
 
     # Walk Source folder and detect pictures
-    source_pictures = get_source_material(source_path)
-    print(source_pictures)
-    organize(source_pictures, target_folder, default_folder)
-
+    messy_pictures = find_photos(opts.source_path)
+    # unmess the library
+    organize(messy_pictures, opts.target_folder, opts.default_folder)
 
 
 if __name__ == '__main__':
